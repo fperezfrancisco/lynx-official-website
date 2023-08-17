1d2530a65297a6ff9606517db130f637fe2ea81e
Francisco Perez <fperezfrancisco4@gmail.com>
2023-08-17 15:08:43 -0700

    fixed styling and contact page

--- script/contactPage.js
+++ script/contactPage.js
@@ -1,5 +1,13 @@
 const contactForm = document.querySelector("#contactForm");
 const sendMessageBtn = document.querySelector("#sendMessageBtn");
+const formItems = document.querySelectorAll(".formItem");
+const thankYouContainer = document.querySelector(".thankYouContainer");
+
+formItems.forEach((formItem) => {
+  formItem.classList.remove("hidden");
+});
+sendMessageBtn.classList.remove("hidden");
+
 const scriptURL =
   "https://script.google.com/macros/s/AKfycbwOv6tPZzaCZXB0EZvc6svxptVm9-9gkINuqCkwDmnRYsQnBNvFi8JKr0DF_ZfyEdFX/exec";
 
@@ -9,7 +17,13 @@ contactForm.addEventListener("submit", (e) => {
   let requestBody = new FormData(contactForm);
   fetch(scriptURL, { method: "POST", body: requestBody })
     .then((response) => {
-      alert("Success!", response);
+      // change the styles for the form
+      formItems.forEach((formItem) => {
+        formItem.classList.add("hidden");
+      });
+      sendMessageBtn.classList.add("hidden");
+      //alert("Success!", response);
+      thankYouContainer.classList.remove("hidden");
       sendMessageBtn.disabled = false;
     })
     .catch((error) => {
--- script/index.js
+++ script/index.js
@@ -1,6 +1,8 @@
 const mobileMenu = document.querySelector(".mobileMenuContainer");
 const menuOpenBtn = document.querySelector(".mobileMenuOpen");
 const menuCloseBtn = document.querySelector(".mobileMenuClose");
+const subscribeForm = document.querySelector(".subscribeForm");
+const joinBtn = document.querySelector(".subscribeBtn");
 
 function toggleMenu() {
   mobileMenu.classList.toggle("active");
@@ -18,4 +20,26 @@ document.addEventListener("scroll", () => {
   } else {
     mainHeader.classList.remove("scrolling");
   }
+});
+
+const subscribeURL =
+  "https://script.google.com/macros/s/AKfycbzDn9C4ewnDUupstp7uNdrmaODpzqKvYN6dGvQVXAc-i6DXkQuAi06H7_EJVei_VlGE/exec";
+
+subscribeForm.addEventListener("submit", (e) => {
+  joinBtn.disabled = true;
+  console.log("entered event listenter");
+  e.preventDefault();
+  let requestBody = new FormData(subscribeForm);
+  fetch(subscribeURL, { method: "POST", body: requestBody })
+    .then((response) => {
+      // change the styles for the form
+      //sendMessageBtn.classList.add("hidden");
+      alert("Success!", response);
+      //thankYouContainer.classList.remove("hidden");
+      joinBtn.disabled = false;
+    })
+    .catch((error) => {
+      alert("Error!", error.message);
+      joinBtn.disabled = false;
+    });
 });
